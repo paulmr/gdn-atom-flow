f07e0c68687747a945d9a1b7c9b6d81089515386
Paul Roberts <paul.roberts@guardian.co.uk>
2016-01-07 14:26:22 +0000

    refactor
    make it into a sbt-resolver startable background process
    
    implement some simple (slf4j-based) logging

---
+++ src/main/scala/AtomFlow.scala
@@ -0,0 +1,72 @@
+package atomflow
+
+import scala.io.Source
+import java.io.{ FileWriter, File }
+import java.nio.ByteBuffer
+import scala.concurrent.Await
+import scala.concurrent.duration.{ Duration, SECONDS }
+import com.typesafe.config.Config
+import com.typesafe.scalalogging.LazyLogging
+import com.gu.contentatom.thrift
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+/* simple cli tool */
+
+class AtomFlow(config: Config) extends Thread
+    with RecordProcessor
+    with LazyLogging {
+
+  val kinesis = new KinesisReader(config)
+
+  private var active = true
+
+  lazy val checkpointFile = {
+    if(config.hasPath("checkpointfile"))
+      Some(new File(config.getString("checkpointfile")))
+    else
+      None
+  }
+
+  def readCheckpointFile(): Option[String] = checkpointFile flatMap { file =>
+    if(file.exists()) {
+      Some(Source.fromFile(file).mkString)
+    } else {
+      logger.warn(s"Checkpoint file $file doesn't exist")
+      None
+    }
+  }
+
+  def writeCheckpoint(sq: String): Unit = checkpointFile foreach { file =>
+    val out = new FileWriter(file)
+    out.write(sq, 0, sq.length)
+    out.close()
+    logger.info(s"Wrote checkpoint to $file")
+  }
+
+
+  override def run = {
+    logger.info(s"Connecting to: ${kinesis.streamName}")
+
+    var iterator =
+      readCheckpointFile().map(kinesis.newIteratorFrom(_)) getOrElse kinesis.newIteratorAll()
+
+    while(active) {
+      val res = kinesis.getRecords(iterator)
+      res.records.foreach(processRecord _)
+      // if we have a new sequence number, write it to the checkpoint
+      res.sequenceNum.foreach(writeCheckpoint _)
+      // delay before we try again
+      if(active) {
+        val seconds = 5
+        logger.info(s"Sleeping for $seconds seconds")
+        Thread.sleep(seconds * 1000)
+      }
+      iterator = res.nextIterator
+    }
+  }
+
+  def shutdown = {
+    active = false
+  }
+}
